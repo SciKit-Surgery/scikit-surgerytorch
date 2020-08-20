d651d838d71b604645d65e5b749a1a7b0d5f15bd
Thomas Dowrick <tdowrick@gmail.com>
2020-08-20 09:40:08 +0100

    Issue #1: First implementation of V2S network and tests

---
+++ tests/test_v2s.py
@@ -0,0 +1,36 @@
+"""HSMNet Tests."""
+import os
+import pytest
+import cv2
+import numpy as np
+import torch
+from sksurgerytorch.models.volume_to_surface import Volume2SurfaceCNN
+
+#pylint:disable=not-callable,invalid-name
+def test_v2snet_no_weights():
+    """ This won't give a good results, but we can check that the network
+    at least runs ok. """
+    
+    grid_size = 64
+    num_elements = grid_size*grid_size*grid_size
+    V2SNet = Volume2SurfaceCNN(grid_size=grid_size)
+
+    preop_signed = np.random.random(num_elements) - 0.5
+    intraop_unsigned = np.random.random(num_elements)
+
+    V2SNet.predict(preop_signed, intraop_unsigned)
+
+weights = ('tests/data/weights/v2snet_checkpoint.tar')
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not found")
+@pytest.mark.skipif(not os.path.exists(weights), reason="Weights not found")
+def test_v2snet_with_weights():
+
+    """ Test with some (non-trained) weights."""
+    grid_size = 64
+    num_elements = grid_size*grid_size*grid_size
+    V2SNet = Volume2SurfaceCNN(grid_size=grid_size, weights=weights)
+
+    preop_signed = np.random.random(num_elements) - 0.5
+    intraop_unsigned = np.random.random(num_elements)
+
+    V2SNet.predict(preop_signed, intraop_unsigned)
